c892275de87d1e25af689740027791927ed61122
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-15 15:07:26 -0300

    comments

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import { useState, useEffect } from "react";
 import { SearchBar } from "./components/SearchBar";
 
 function App() {
+  // Temos dois states aqui: um para armazenamento principal e outro para backup
   const [pets, setPets] = useState([]);
   const [backup, setBackup] = useState([]);
 
+  // useEffect para preencher ambos os states (principal e backup)
   useEffect(() => {
     async function fetchPets() {
-      const response = await axios.get(
-        "https://ironrest.herokuapp.com/catchapet-agoravai"
-      );
+      const response = await axios.get("END POINT DA API AQUI");
+
+      // Setando ambos os states
       setBackup([...response.data]);
       setPets([...response.data]);
     }
 
+    // Invocando nossa função assincrona dentro da callback do useEffect
     fetchPets();
   }, []);
 
+  // Essa é a função responsavel por filtrar o state
   function filterPets(searchParams) {
+    // Caso o value do input esteja vazio, voltamos o valor do state principal para o valor do backup
     if (searchParams === "") {
       setPets([...backup]);
       return;
     }
 
+    // Filtrando o state principal para verificar se inclui o que foi digitado no input
     const filtred = pets.filter((currentPet) =>
       currentPet.name.toLowerCase().includes(searchParams.toLowerCase())
     );
+
+    // Atualizando o state com o array filtrado acima
     setPets(filtred);
   }
 
   return (
     <>
+      {/* Passando a função de filtrar o state principal para o componente SearchBar */}
+      {/* Essa função será invocada no evento onKeyUp */}
       <SearchBar filterAPI={filterPets} />
       {pets.map((currentPet) => (
         <div>{`${currentPet.name}, ${currentPet.species}`}</div>
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,6 +4,7 @@ export function SearchBar(props) {
       type="text"
       name="searchParams"
       placeholder="O que você procura?"
+      // Criando uma callback anonima para invocar a função passada por props com o value do input como parametro
       onKeyUp={(event) => {
         props.filterAPI(event.target.value);
       }}
